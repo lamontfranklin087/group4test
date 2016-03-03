98c31517abcc82da75336d8e2b00f37ca9b7a826
David H <dave1729@uw.edu>
2016-03-02 16:03:33 -0800

    minor spelling changes

--- NPS/src/view/UI.java
+++ NPS/src/view/UI.java
@@ -143,7 +143,7 @@ public class UI {
 	        		break;
 	        	}
 	        } catch(NumberFormatException ne) {
-	            System.out.println("That's not a write number.");	            
+	            System.out.println("That's not a valid number.");	            
 	        }	
 		}
 		return result;
