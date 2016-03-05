c69019f881f609cb7a4cec04acded8de0c54dfc1
iharL <lavor_igorusa@yahoo.com>
2016-03-05 00:50:19 -0800

    Finished UrbanParksStaff test class.

--- NPS/src/test/UrbanParksStaffTest.java
+++ NPS/src/test/UrbanParksStaffTest.java
@@ -5,11 +5,18 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import model.Job;
+import model.UrbanParksStaff;
+import model.User;
+import model.Volunteer;
 
 /**
  * @author Student
@@ -17,40 +24,69 @@ import org.junit.Test;
  */
 public class UrbanParksStaffTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
+	private final int NUM_OF_TEST_JOBS = 10;
+	
+	private Collection<User> testUsers;
+	
+	private Collection<Job> testJobs;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
+		Job tempJob;
+		Volunteer testUser1 = new Volunteer("JUnitTestFirst1", "JUnitTestLast1",
+								 "JUnitTestEmail1", "JUnitTestPassword1");
+		Volunteer testUser2 = new Volunteer("JUnitTestFirst2", "JUnitTestLast2",
+				 "JUnitTestEmail2", "JUnitTestPassword2");
+		testJobs = new LinkedList<Job>();
+		testUsers = new LinkedList<User>();
+		for (int i = 0; i < NUM_OF_TEST_JOBS; i++) {
+			
+			int day = 5 + i;
+			int month = 4;
+			int year = 2016;
+			
+			Calendar jobDate = new GregorianCalendar();			
+			jobDate.set(Calendar.YEAR, year);
+			jobDate.set(Calendar.MONTH, month);
+			jobDate.set(Calendar.DAY_OF_MONTH, day);
+			
+			tempJob = new Job();			
+			tempJob.setDate(jobDate);
+			tempJob.setJobDuration(1);
+			tempJob.setJobSlot(5, 5, 5);
+			tempJob.setJobID(i + 1);
+			tempJob.setJobDescription("TestJob#:" + (i + 1));
+			testJobs.add(tempJob);
+		}
+		int desiredJobID = 4;
+		int desiredSlot = 1;
+		((Volunteer)testUser1).jobSignUp(testJobs, desiredJobID, desiredSlot);
+		testUsers.add(testUser1);
+		((Volunteer)testUser1).jobSignUp(testJobs, desiredJobID + 1, desiredSlot);
+		testUsers.add(testUser2);
 	}
 
 	/**
 	 * Test method for {@link model.UrbanParksStaff#searchVolunteer(java.util.Collection, java.lang.String)}.
 	 */
 	@Test
-	public void testSearchVolunteer() {
-		fail("Not yet implemented");
+	public void testSearchVolunteerExisting() {
+		UrbanParksStaff testUser = new UrbanParksStaff("StaffFirst", "StaffLast",
+				 "StaffEmail", "StaffPassword");
+		User tempVol = ((UrbanParksStaff)testUser).searchVolunteer(testUsers, "JUnitTestLast1");
+		assertEquals(tempVol.getLastName(), "JUnitTestLast1");
 	}
 
+	/**
+	 * Test method for {@link model.UrbanParksStaff#searchVolunteer(java.util.Collection, java.lang.String)}.
+	 */
+	@Test
+	public void testSearchVolunteerNotExisting() {
+		UrbanParksStaff testUser = new UrbanParksStaff("StaffFirst", "StaffLast",
+				 "StaffEmail", "StaffPassword");
+		User tempVol = ((UrbanParksStaff)testUser).searchVolunteer(testUsers, "notExistLast1");
+		assertEquals(tempVol, null);
+	}
 }
