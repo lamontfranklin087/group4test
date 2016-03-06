6ad6e84990005f8727c194a6896ec6c28b7e3fe0
David H <dave1729@uw.edu>
2016-03-06 04:17:50 -0800

    Added some Job Tests.

--- NPS/src/test/JobTest.java
+++ NPS/src/test/JobTest.java
@@ -5,17 +5,26 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import controller.CheckBusinessRules;
+import model.Job;
+import model.MyOwnException;
+
 /**
  * @author Student
  *
  */
 public class JobTest {
+	
+	private Job testJob;
 
 	/**
 	 * @throws java.lang.Exception
@@ -36,6 +45,7 @@ public class JobTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		testJob = new Job();
 	}
 
 	/**
@@ -47,18 +57,60 @@ public class JobTest {
 
 	/**
 	 * Test method for {@link model.Job#setJobLocation(java.lang.String)}.
+	 * @throws MyOwnException 
 	 */
 	@Test
-	public void testSetJobLocation() {
-		fail("Not yet implemented");
+	public void testSetJobLocation() throws MyOwnException {
+		String actualLocation, expectedLocation = "testLocation";
+		testJob.setJobLocation(expectedLocation);
+		actualLocation = testJob.getJobLocation();
+		assertEquals("setLocation failed.", expectedLocation, actualLocation);
 	}
 
 	/**
 	 * Test method for {@link model.Job#setDate(java.util.Calendar)}.
+	 * @throws MyOwnException 
 	 */
 	@Test
-	public void testSetDate() {
-		fail("Not yet implemented");
+	public void testSetDatePresent() throws MyOwnException {
+		Calendar actualDate;
+		Calendar today = new GregorianCalendar();
+		
+		testJob.setDate(today);
+		actualDate = testJob.getDate();
+		assertEquals("setLocation failed.", today, actualDate);
+	}
+
+	/**
+	 * Test method for {@link model.Job#setDate(java.util.Calendar)}.
+	 * @throws MyOwnException 
+	 */
+	@Test(expected = MyOwnException.class)
+	public void testSetDatePast() throws MyOwnException{
+		//make a calendar set to today
+		Calendar yesterday = new GregorianCalendar();
+		
+		//roll that back one day to yesterday
+		yesterday.roll(Calendar.DATE, false);
+		
+		//this line should then throw the expected exception
+		testJob.setDate(yesterday);
+	}
+	
+	/**
+	 * Test method for {@link model.Job#setDate(java.util.Calendar)}.
+	 * @throws MyOwnException 
+	 */
+	@Test(expected = MyOwnException.class)
+	public void testSetDateFuture() throws MyOwnException{
+		//make a calendar set to today
+		Calendar threeMonthsLater = new GregorianCalendar();
+		
+		//roll that forward MAX_FUTURE_DAYS_FOR_NEW_JOBS + 1 days
+		threeMonthsLater.roll(Calendar.DAY_OF_YEAR, 91);
+		
+		//this line should then throw the expected exception
+		testJob.setDate(threeMonthsLater);
 	}
 
 	/**
